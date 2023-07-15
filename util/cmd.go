d317f0408f91b8aeb04bfb01300af46512469701
sshelll <shaojiale.gitignore@icloud.com>
2023-07-15 20:00:38 +0800

    feat: support find / exec the closest test with the given pos. this could be useful to nvim plug.

---
+++ util/cmd.go
@@ -0,0 +1,50 @@
+package util
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func BuildTestAllExpr(testList []string) string {
+	buf := strings.Builder{}
+	cnt := len(testList)
+
+	for i, testName := range testList {
+		buf.WriteString("^")
+		buf.WriteString(testName)
+		buf.WriteString("$")
+		if i < cnt-1 {
+			buf.WriteString("\\|")
+		}
+	}
+
+	return buf.String()
+}
+
+func ExecGoTest(testName string, flags ...string) {
+	args := bytes.Buffer{}
+	flagLen := len(flags)
+	for i := 0; i < flagLen; i++ {
+		args.WriteString(flags[i])
+		args.WriteString(" ")
+	}
+
+	goTestCmd := fmt.Sprintf("go test %s -test.run %s", args.String(), testName)
+	execCmd(goTestCmd, true)
+}
+
+func execCmd(sh string, useStdIO bool) {
+	cmd := exec.Command("bash", "-c", sh)
+	if useStdIO {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("[gott] exec cmd '%s' failed, err = %v\n", cmd.String(), err)
+	}
+}
