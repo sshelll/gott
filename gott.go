738ab241b62e0d9f8f34c0b4725b8c838e651f43
sshelll <shaojiale.gitignore@icloud.com>
2023-07-16 14:58:52 +0800

    feat: support --runFile flag

--- gott.go
+++ gott.go
@@ -9,15 +9,7 @@ import (
 	"github.com/sshelll/sinfra/ast"
 )
 
-// lua print(require'util'.get_pos_under_cursor())
-// lua print(require'gott'.get_test_name('/Users/shaojiale/Codes/github/mine/gott/util/mock_test.go', 423))
-
 func main() {
-	var (
-		testName string
-		done     bool
-	)
-
 	switch true {
 	// help
 	case len(os.Args) == 2 && (os.Args[1] == "-h" || os.Args[1] == "--help"):
@@ -26,7 +18,7 @@ func main() {
 
 	// pos
 	case len(os.Args) > 1 && os.Args[1] == "--pos":
-		testName, done = posMode()
+		testName, done := posMode()
 		if done {
 			return
 		}
@@ -34,11 +26,31 @@ func main() {
 			print(testName)
 			return
 		}
-		util.ExecGoTest(testName, os.Args[3:]...)
+		var args []string
+		if len(os.Args) > 3 {
+			args = os.Args[3:]
+		}
+		util.ExecGoTest(testName, args...)
+
+	// run file
+	case len(os.Args) > 1 && os.Args[1] == "--runFile":
+		testName, done := runFileMode()
+		if done {
+			return
+		}
+		if len(os.Args) > 3 && os.Args[3] == "-p" {
+			print(testName)
+			return
+		}
+		var args []string
+		if len(os.Args) > 3 {
+			args = os.Args[3:]
+		}
+		util.ExecGoTest(testName, args...)
 
 	// interactive
 	default:
-		testName, done = interactiveMode()
+		testName, done := interactiveMode()
 		if done {
 			return
 		}
@@ -46,7 +58,11 @@ func main() {
 			print(testName)
 			return
 		}
-		util.ExecGoTest(testName, os.Args[1:]...)
+		var args []string
+		if len(os.Args) > 1 {
+			args = os.Args[1:]
+		}
+		util.ExecGoTest(testName, args...)
 	}
 }
 
@@ -68,6 +84,15 @@ func helpMode() {
 		"\t\t'gott --pos xxx_test.go:59 -p'\n" +
 		"\t\tIn this way, gott would print the closest test name of the uri\n" +
 		"\t\tPlease note that if you want to use --pos and -p together, you should put the --pos in the first, uri in the sec, and -p is the third\n" +
+		"\t\t Use --runFile to exec all test in a file\n" +
+		"\t\tNOTE: this flag must be the first arg if you try to use it!!!\n" +
+		"\t\tFor example: \n" +
+		"\t\t'gott --runFile /Users/sshelll/go/src/gott/xxx_test.go'\n" +
+		"\t\tIn this way, gott would try to exec all test in the file\n" +
+		"\t\t'gott --runFile /Users/sshelll/go/src/gott/xxx_test.go -v'\n" +
+		"\t\tIn this way, gott would try to exec all test in the file with -v flag\n" +
+		"\t\t'gott --runFile /Users/sshelll/go/src/gott/xxx_test.go -p'\n" +
+		"\t\tIn this way, gott would print all test name in the file\n" +
 		"\nOtherwise you will exec go test with interactive mode, and other args will be passed to 'go test'\n" +
 		"\tFor example: \n" +
 		"\t\t'gott' equals 'go test'\n" +
@@ -134,5 +159,23 @@ func posMode() (testName string, done bool) {
 
 	testName = fmt.Sprintf("^%s$", testName)
 
+	return
+}
+
+func runFileMode() (testName string, done bool) {
+	f := os.Args[2]
+	fInfo, err := ast.Parse(f)
+	if err != nil {
+		log.Fatalln("[gott] ast parse failed:", err.Error())
+	}
+
+	goTests := util.ExtractTestFuncs(fInfo)
+
+	if len(goTests) == 0 {
+		log.Println("[gott] no tests were found, exit...")
+		return "", true
+	}
+
+	testName = util.BuildTestAllExpr(goTests)
 	return
 }
